fc31770e581662f8894b1e612a6e47abfd0183db
MABS <avrysou@gmail.com>
2018-08-10 21:45:01 -0400

    updated welcome screen with typing animation and reduced word count

--- scripts/day-night.js
+++ scripts/day-night.js
@@ -2,9 +2,11 @@
 //Get time zone hours to determine if it's morning, afternoon or evening
 var currentTime = new Date();
 var hours = currentTime.getHours();
+var text = 'Hello';
 
 if (hours >= 0 && hours < 12) {
-  document.getElementById("intro-greeting").innerHTML = "&#x1F305 Good Morning";
+  // document.getElementById("intro-greeting").innerHTML = 'Good Morning';
+  text ='Good Morning';
   $('u').addClass('morning');
   $('u').removeClass('afternoon');
   $('u').removeClass('evening');
@@ -14,7 +16,8 @@ if (hours >= 0 && hours < 12) {
 }
 
 if (hours >= 12 && hours < 17) {
-  document.getElementById("intro-greeting").innerHTML = "&#x1F3DE Good Afternoon";
+  // document.getElementById("intro-greeting").innerHTML = "Good Afternoon";
+  text ='Good Afternoon';
   $('u#intro-greeting').removeClass('morning');
   $('u#intro-greeting').addClass('afternoon');
   $('u#intro-greeting').removeClass('evening');
@@ -23,8 +26,9 @@ if (hours >= 12 && hours < 17) {
   $('a.portfolio-embeded-link').removeClass('evening');
 }
 
-if (hours >= 17 && hours < 20) {
-  document.getElementById("intro-greeting").innerHTML = " &#x1F307 Good Evening";
+if (hours >= 17 && hours <= 24) {
+  // document.getElementById("intro-greeting").innerHTML = "Good Evening";
+  text ='Good Evening';
   $('u').removeClass('morning');
   $('u').removeClass('afternoon');
   $('u').addClass('evening');
@@ -33,12 +37,26 @@ if (hours >= 17 && hours < 20) {
   $('a.portfolio-embeded-link').addClass('evening');
 }
 
-if (hours >= 20 && hours <= 24) {
-  document.getElementById("intro-greeting").innerHTML = " &#x1F303 Good Evening";
-  $('u').removeClass('morning');
-  $('u').removeClass('afternoon');
-  $('u').addClass('evening');
-  $('a.portfolio-embeded-link').removeClass('morning');
-  $('a.portfolio-embeded-link').removeClass('afternoon');
-  $('a.portfolio-embeded-link').addClass('evening');
+document.addEventListener('DOMContentLoaded', startAnimation(event));
+
+function startAnimation(event){
+  // array with texts to type in typewriter
+  var dataText = [text];
+  setTimeout(function () {typeWriter(dataText[0], 0)}, 1000);
+};
+
+setTimeout(function () {$("header").removeClass("onload")}, 25);
+
+
+
+function typeWriter(text, i) { // type one letter/symbol and continue calling itself until the text is finished
+   // check if any letters are left
+   if (i < (text.length)) {
+      // add next character to h1
+      document.getElementById("intro-greeting").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1)
+      }, 100);
+   }
 }
